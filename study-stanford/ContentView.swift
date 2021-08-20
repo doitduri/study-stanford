7d925fb6b2500ad561c6e9d0f1c10cc4a717e625
doitduri <kdle_@naver.com>
2021-08-20 15:06:52 +0900

    📚 lecture 1: Getting started with SwiftUI
    - 기본 Xcode 구조 설명
    - SwiftUI View concept 설명

--- study-stanford/ContentView.swift
+++ study-stanford/ContentView.swift
@@ -9,10 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            RoundedRectangle(cornerRadius: 25.0).stroke(lineWidth: 3)
+            Text("Hello world")
+        }
+        .padding(.horizontal)
+        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
     }
 }
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {

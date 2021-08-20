473f69d30246191397348aa999b8cf76f3d01eb4
doitduri <kdle_@naver.com>
2021-08-20 14:23:49 +0900

    Initial Commit

---
+++ study-stanford/study_stanfordApp.swift
@@ -0,0 +1,17 @@
+//
+//  study_stanfordApp.swift
+//  study-stanford
+//
+//  Created by 김두리 on 2021/08/20.
+//
+
+import SwiftUI
+
+@main
+struct study_stanfordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

d11f0ec72e9aa4d969b4b603c2d88d3614cb2ac0
doitduri <kdle_@naver.com>
2021-08-20 15:21:40 +0900

    📚 lecture 2:  View
    - 커스텀 View를 할 경우, 상속 받으면 됨
    - View는 프로토콜 형식으로 정의 되어 있음

--- study-stanford/ContentView.swift
+++ study-stanford/ContentView.swift
@@ -9,16 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 25.0).stroke(lineWidth: 3)
-            Text("Hello world")
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
         }
         .padding(.horizontal)
         .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
     }
 }
 
-
+struct CardView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25.0).stroke(lineWidth: 3)
+            Text("Hello world")
+        }
+    }
+}
 
 
 
